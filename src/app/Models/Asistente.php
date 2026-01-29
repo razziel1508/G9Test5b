353e9af46542240f513ec313ef830a49bc4bfe84
Algorithmic Research & Tech Solutions (ART Solutions) <53839636+razziel1508@users.noreply.github.com>
2026-01-29 23:38:49 +0000

    primer guardado

---
+++ src/app/Models/Asistente.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Asistente extends Model
+{
+    protected $table = 'asistentes';
+    protected $fillable = [
+        'nombre',
+        'email',
+        'telefono',
+        'evento_id',
+    ];
+}
---
+++ src/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+use App\Http\Controllers\PonenteController;
+use App\Http\Controllers\AsistenteController;
+
+
+Route::apiResource('ponentes', PonenteController::class);
+Route::apiResource('asistentes', AsistenteController::class);
